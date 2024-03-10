215f3526079bc63b0bb9358444cb7d25d602207f
Emil Huseynaliev <emilh1791@gmail.com>
2024-03-09 19:28:09 -0500

    Add Spanish and Russian translations
    Signed-off-by: Emil Huseynaliev <emilh1791@gmail.com>

--- strings/index.js
+++ strings/index.js
@@ -1,6 +1,27 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import en from './en';
+import es from './es';
+import ru from './ru';
 
-let currStringSource = en; // default
+const LANGUAGES = [
+  {id:'en', name:'English', src:en},
+  {id:'es', name:'Español', src:es},
+  {id:'ru', name:'Русский', src:ru},
+];
+
+const LANGS_BY_ID = {};
+for (const lang of LANGUAGES) {
+  LANGS_BY_ID[lang.id] = lang;
+}
+
+let currLangId = 'en'; // TODO: read from system or storage
+
+loadLangFromStorage();
+
+async function loadLangFromStorage() {
+  const langId = await AsyncStorage.getItem('lang');
+  return switchToLang(langId);
+}
 
 /**
  * Localize a string (given a key)
@@ -10,5 +31,26 @@ let currStringSource = en; // default
  * @returns string - the localized string
  */
 export function t(strKey) {
-  return currStringSource[strKey];
+  const src = LANGS_BY_ID[currLangId].src;
+  return src[strKey];
+}
+
+export function* getSupportedLanguages() {
+  for (const {id, name} of LANGUAGES) {
+    yield {id, name};
+  }
+}
+
+export function getCurrentLangId() {
+  return currLangId;
+}
+
+export function switchToLang(langId) {
+  if (langId in LANGS_BY_ID) {
+    currLangId = langId;
+    AsyncStorage.setItem('lang', langId); // no need to await
+    return true;
+  }
+
+  return false;
 }
